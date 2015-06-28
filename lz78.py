a2cabd533b27a6342b2b043536d5c4eb1b546884
hpanago <hpanago@gmail.com>
2015-06-28 23:57:33 +0300

    Size still not ok, debugging messages still in the code.

--- lz78.py
+++ lz78.py
@@ -1,19 +1,76 @@
+import pickle
+
 file_input = raw_input();
 dictionary = {};
+dictionary2 = {};
 position = 1;
 buf = "";
-file_input += "\EOF";
+tupl = [];
+#file_input += "\EOF";
 for c in file_input:
+    if c is None:  
+        break;
     buf += c;
     if buf not in dictionary:
         dictionary[buf] = position;
         position += 1;
+        #print buf
         if len(buf) == 1:
-            tupl = (0,buf);
+            tupl += (0,buf);
         else:
             char = buf[:-1];
             pos = dictionary[char];
-            tupl = (pos, buf[-1]);
+            dictionary2[pos] = char;
+            tupl += (pos, buf[-1]);
         buf = "";
-        print tupl;
-print dictionary;
+        
+# print dictionary
+# print dictionary2       
+with open('out.txt', 'wb') as handle1:
+    pickle.dump(tupl, handle1)
+
+with open('out.txt', 'rb') as handle1:
+    b1 = pickle.loads(handle1.read())
+
+with open('out1.txt', 'wb') as handle:
+  pickle.dump(dictionary2, handle)
+
+with open('out1.txt', 'rb') as handle:
+  b = pickle.loads(handle.read())
+#print "NOW THE OUTPUT", b
+
+
+#print "NOW THE TUPLE", b1
+#print b1
+counter = 0;
+string = ""
+strin1g = ""
+#print b, b1
+#print len(b1)/2
+for i in range(0,len(b1)/2):
+    if tupl[counter] == 0:
+        #print "COUNTERRR TUPE:E ", tupl[counter] 
+        string += tupl[counter +1]
+        counter += 2;
+    else:
+        #print tupl[counter]
+        #if i == int():
+        string += b[tupl[counter]] + tupl[counter+1]
+        counter +=2;
+print string
+
+
+for i in range(0,len(b1)/2):
+    if tupl[i] == 0:
+        #print "COUNTERRR TUPE:E ", tupl[counter] 
+        strin1g += tupl[i+1]
+        
+#print strin1g
+
+test= ""
+for i in dictionary2:
+    test += dictionary2[i];
+    # if tupl[counter] == 0:
+    #     test += tupl[counter+1]
+    #     counter += 2; 
+#print test
