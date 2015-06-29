e46b7c8125db31c3dc665542dd57f652d7d0b6a8
hpanago <hpanago@gmail.com>
2015-06-29 18:44:31 +0300

    First step in decompression

--- lz78.py
+++ lz78.py
@@ -6,7 +6,8 @@ dictionary2 = {};
 position = 1;
 buf = "";
 tupl = [];
-#file_input += "\EOF";
+lista = [];
+#now the EOF happens when we meet a new paragraph
 for c in file_input:
     if c is None:  
         break;
@@ -14,63 +15,25 @@ for c in file_input:
     if buf not in dictionary:
         dictionary[buf] = position;
         position += 1;
-        #print buf
         if len(buf) == 1:
             tupl += (0,buf);
         else:
             char = buf[:-1];
             pos = dictionary[char];
-            dictionary2[pos] = char;
             tupl += (pos, buf[-1]);
         buf = "";
-        
-# print dictionary
-# print dictionary2       
-with open('out.txt', 'wb') as handle1:
-    pickle.dump(tupl, handle1)
+outputFile = open('out.txt','wr')
+for item in tupl:
+  outputFile.write("%s\n" % item)
 
-with open('out.txt', 'rb') as handle1:
-    b1 = pickle.loads(handle1.read())
-
-with open('out1.txt', 'wb') as handle:
-  pickle.dump(dictionary2, handle)
-
-with open('out1.txt', 'rb') as handle:
-  b = pickle.loads(handle.read())
-#print "NOW THE OUTPUT", b
-
-
-#print "NOW THE TUPLE", b1
-#print b1
-counter = 0;
-string = ""
-strin1g = ""
-#print b, b1
-#print len(b1)/2
-for i in range(0,len(b1)/2):
-    if tupl[counter] == 0:
-        #print "COUNTERRR TUPE:E ", tupl[counter] 
-        string += tupl[counter +1]
-        counter += 2;
+for i in  range(0,len(tupl),2):  
+    if tupl[i] == 0:
+        lista.append(tupl[i+1])
     else:
-        #print tupl[counter]
-        #if i == int():
-        string += b[tupl[counter]] + tupl[counter+1]
-        counter +=2;
+        prefix = lista[tupl[i]-1]
+        lista.append(prefix + tupl[i+1])
+string = ""
+for i in lista:
+    string += i;
 print string
 
-
-for i in range(0,len(b1)/2):
-    if tupl[i] == 0:
-        #print "COUNTERRR TUPE:E ", tupl[counter] 
-        strin1g += tupl[i+1]
-        
-#print strin1g
-
-test= ""
-for i in dictionary2:
-    test += dictionary2[i];
-    # if tupl[counter] == 0:
-    #     test += tupl[counter+1]
-    #     counter += 2; 
-#print test
